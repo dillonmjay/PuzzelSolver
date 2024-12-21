7c82f9eb139ae9a7d2c46a91449422949d531385
Dillon Jayatillake <dillonmichaeljay@gmail.com>
2024-12-21 23:12:05 +0530

    Update GridPosition.java

--- src/GridPosition.java
+++ src/GridPosition.java
@@ -1,4 +1,14 @@
 
 public class GridPosition {
 
+    private int x; // Row index
+    private int y; // Column index
+
+    public GridPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    
+
 }
